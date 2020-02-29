fae6cca79d5e5b27eca04276658b146ec73abb96
jaspreet <jaspreet.singh@allocatesoftware.com>
2020-03-01 01:45:15 +0530

    Updates

---
+++ precache-manifest.7ba8d9df8642a87bc7db8cba29a37cea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97f44b8a14fced51f68552e2b04c01f",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "e25c7117a2bb722a1090",
+    "url": "/star-wars/static/css/main.070636b7.chunk.css"
+  },
+  {
+    "revision": "519c724c2d85ee73ec22",
+    "url": "/star-wars/static/js/2.95afa6d6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/star-wars/static/js/2.95afa6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25c7117a2bb722a1090",
+    "url": "/star-wars/static/js/main.65c3e08f.chunk.js"
+  },
+  {
+    "revision": "9a8f41536a9611460178",
+    "url": "/star-wars/static/js/runtime-main.21a762a3.js"
+  },
+  {
+    "revision": "72cf2e802eb32ca14d9868fb2005909c",
+    "url": "/star-wars/static/media/Verdana.72cf2e80.ttf"
+  },
+  {
+    "revision": "573f2ad137cbdd2ba199e7189d5b733d",
+    "url": "/star-wars/static/media/darth.573f2ad1.png"
+  }
+]);
